5adef60ed5fad339ac6cb5e427b1e409a307ab58
spiderman2billion <indoorplumbing@mac.com>
2017-05-27 22:40:06 -0500

    update

--- zsh/_path.zsh
+++ zsh/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$ZSH_HOME/bin:$HOME/bin/scripts:$PATH"
+export PATH="$ZSH_HOME/bin:$HOME/bin/scripts:$PATH:/usr/local/sbin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
